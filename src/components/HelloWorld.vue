6f472cdfa040606f89ad235e13073bb96b954498
Mariana Hiath <mari.dsn.pack@alterdata.com.br>
2022-05-23 17:00:50 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>oi</h1>
+</template>
+
+<script>
+export default {
+  
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,112 @@
+<template>
+  <main >
+    <section class="columns wrapped">
+      <div class="column square is-3 text box">
+        <p class="text-left is-size-5">Matriz</p>
+        <p class="is-size-2 text-bold mb-2 has-text-info-dark">1227</p>
+        <div class="flex">
+          <div>
+            <p class="is-size-5 mb-1">sim</p>
+            <p class="is-size-4 has-text-weight-semibold">70%</p>
+          </div>
+          <div class="separator"></div>
+          <div>
+            <p class="is-size-5 mb-1">não</p>
+            <p class="is-size-4 has-text-weight-semibold">30%</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column square is-3 text box">
+          <p class="text-left is-size-5">Filiais</p>
+        <p class="is-size-2 text-bold mb-2 has-text-info-dark">1227</p>
+        <div class="flex">
+          <div>
+            <p class="is-size-5 mb-1">sim</p>
+            <p class="is-size-4 has-text-weight-semibold">70%</p>
+          </div>
+          <div class="separator"></div>
+          <div>
+            <p class="is-size-5 mb-1">não</p>
+            <p class="is-size-4 has-text-weight-semibold">30%</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column square is-3 text box">
+        <p class="text-left is-size-5">Geral</p>
+        <p class="is-size-2 text-bold mb-2 has-text-info-dark">1227</p>
+        <div class="flex">
+          <div>
+            <p class="is-size-5 mb-1">sim</p>
+            <p class="is-size-4 has-text-weight-semibold">70%</p>
+          </div>
+          <div class="separator"></div>
+          <div>
+            <p class="is-size-5 mb-1">não</p>
+            <p class="is-size-4 has-text-weight-semibold">30%</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .wrapped {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 150px;
+    height: 100vh;
+  }
+
+  .square {
+    width: 280px;
+    height: 200px;
+    border-radius: 8px;
+    background: #fff;
+
+    padding: 0 30px;
+
+    transition: all 1s;
+  }
+
+  .square:hover {
+    transform: scale(1.1);
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .text-left {
+    align-self: flex-start;
+  }
+
+  .text-bold {
+    font-weight: bolder;
+  }
+
+  .separator {
+    width: 1.3px;
+    height: 40px;
+    background: 	#1d72aa;
+  }
+
+  .flex {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+</style>
